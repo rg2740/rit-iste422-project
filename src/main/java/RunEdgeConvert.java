185ff1a0a973d4b886225d78867a1dcb23e14d65
robert graham <rg2740@g.rit.edu>
2020-03-05 15:28:38 -0500

    test

---
+++ src/main/java/RunEdgeConvert.java
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+public class RunEdgeConvert {
+   public static void main(String[] args) {
+      EdgeConvertGUI edge = new EdgeConvertGUI();
+   }
+=======
+public class RunEdgeConvert {
+   public static void main(String[] args) {
+      EdgeConvertGUI edge = new EdgeConvertGUI();
+   }
+>>>>>>> 315461bd285e502d8a7912dd0fab70b7eb8364aa
+}
